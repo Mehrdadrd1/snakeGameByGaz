4452e24796477ab1143ceeda317d003af1f2682e
myusername <myemail>
2023-09-28 18:43:07 +0330

    finished

--- snake.js
+++ snake.js
@@ -17,12 +17,16 @@ const food = {
   y: 300,
 };
 
-const snakeStartDom = document.getElementById("snakeStart");
+const snakeStartDom =
+  document.getElementById("snakeStart");
 
-const snakeBoardDom = document.getElementById("snakeBoard");
+const snakeBoardDom =
+  document.getElementById("snakeBoard");
 
 const convertSecondsTotime = () => {
-  let minutesNumber = Math.floor(seconds / 60);
+  let minutesNumber = Math.floor(
+    seconds / 60
+  );
   let secondsNumber = seconds % 60;
 
   if (minutesNumber < 10) {
@@ -37,16 +41,21 @@ const convertSecondsTotime = () => {
 };
 
 const updateTimeinDOM = () => {
-  document.querySelector("#snakeTime .time").innerHTML =
-    convertSecondsTotime();
+  document.querySelector(
+    "#snakeTime .time"
+  ).innerHTML = convertSecondsTotime();
 };
 
 const updateSnakeinDOM = () => {
   let snakeHTML = ``;
-  snake.positions.forEach((positions, index) => {
-    snakeHTML += `<div class="snakeBox" style="left: ${snake.positions[index].x}px; top:${snake.positions[index].y}px></div>`;
-  });
-  document.getElementById("snake").innerHTML = snakeHTML;
+  snake.positions.forEach(
+    (position, index) => {
+      snakeHTML += `<div class="snakeBox" style="left:${position.x}px; top:${position.y}px"></div>`;
+    }
+  );
+  document.getElementById(
+    "snake"
+  ).innerHTML = snakeHTML;
 };
 
 // document.querySelector("#snakeScore .score").innerHTML =
@@ -55,7 +64,10 @@ const updateSnakeinDOM = () => {
 const startTime = () => {
   if (isCaounting) return;
   isCaounting = true;
-  snake.positions = [{ x: 100, y: 100 }];
+  snake.positions = [
+    { x: 100, y: 100 },
+    { x: 90, y: 100 },
+  ];
   snake.direction = "right";
   timerInterval = setInterval(() => {
     seconds++;
@@ -65,11 +77,15 @@ const startTime = () => {
 
 const generateFood = () => {
   food.x = Math.floor(
-    Math.random() * snakeBoardDom.clientHeight - 10
+    Math.random() *
+      snakeBoardDom.clientHeight -
+      10
   );
   food.x = food.x - (food.x % 10);
   food.y = Math.floor(
-    Math.random() * snakeBoardDom.clientHeight - 10
+    Math.random() *
+      snakeBoardDom.clientHeight -
+      10
   );
   food.y = food.y - (food.y % 10);
   document.getElementById(
@@ -78,8 +94,13 @@ const generateFood = () => {
   document.getElementById(
     "snakeFood"
   ).style.top = `${food.y}px`;
-  document.getElementById("snakeFood").style.display =
-    "block";
+  document.getElementById(
+    "snakeFood"
+  ).style.display = "block";
+
+  document.querySelector(
+    "#snakeScore .score"
+  ).innerHTML = score;
 };
 
 const stopTime = () => {
@@ -89,13 +110,16 @@ const stopTime = () => {
   updateTimeinDOM();
 };
 
-snakeStartDom.addEventListener("click", () => {
-  snakeStartDom.disabled = true;
-  startTime();
-  moveSnake();
-  generateFood();
-  updateSnakeinDOM();
-});
+snakeStartDom.addEventListener(
+  "click",
+  () => {
+    snakeStartDom.disabled = true;
+    startTime();
+    moveSnake();
+    generateFood();
+    updateSnakeinDOM();
+  }
+);
 
 document
   .getElementById("snakeStop")
@@ -113,27 +137,61 @@ const endGame = () => {
 
 const moveSnake = () => {
   snakeInterval = setInterval(() => {
-    if (snake.direction === "right") {
-      snake.positions[0].x += 10;
-    }
-    if (snake.direction === "left") {
-      snake.positions[0].x -= 10;
-    }
-    if (snake.direction === "up") {
-      snake.positions[0].y -= 10;
-    }
-    if (snake.direction === "down") {
-      snake.positions[0].y += 10;
-    }
+    let newSnakePositions = [];
+    const tailPosition =
+      snake.positions[
+        snake.positions.length - 1
+      ];
+    snake.positions.forEach(
+      (position, index) => {
+        if (index === 0) {
+          newSnakePositions.push({
+            x: position.x,
+            y: position.y,
+          });
+          if (
+            snake.direction === "right"
+          ) {
+            newSnakePositions[0].x += 10;
+          }
+          if (
+            snake.direction === "left"
+          ) {
+            newSnakePositions[0].x -= 10;
+          }
+          if (
+            snake.direction === "up"
+          ) {
+            newSnakePositions[0].y -= 10;
+          }
+          if (
+            snake.direction === "down"
+          ) {
+            newSnakePositions[0].y += 10;
+          }
+        } else {
+          newSnakePositions.push(
+            snake.positions[index - 1]
+          );
+        }
+      }
+    );
 
-    const boardHeight = snakeBoardDom.clientWidth;
-    const boardWidth = snakeBoardDom.clientWidth;
+    snake.positions = newSnakePositions;
+
+    const boardHeight =
+      snakeBoardDom.clientWidth;
+    const boardWidth =
+      snakeBoardDom.clientWidth;
 
     if (snake.positions[0].x < 0) {
       endGame();
       return;
     }
-    if (snake.positions[0].x > boardWidth - 10) {
+    if (
+      snake.positions[0].x >
+      boardWidth - 10
+    ) {
       endGame();
       return;
     }
@@ -141,27 +199,61 @@ const moveSnake = () => {
       endGame();
       return;
     }
-    if (snake.positions[0].y > boardHeight - 10) {
+    if (
+      snake.positions[0].y >
+      boardHeight - 10
+    ) {
       endGame();
       return;
+    }
+
+    snake.positions.forEach(
+      (position, index) => {
+        if (index === 0) return;
+        if (
+          snake.positions[0].x ===
+            position.x &&
+          snake.positions[0].y ===
+            position.y
+        ) {
+          endGame();
+          return;
+        }
+      }
+    );
+
+    if (
+      snake.positions[0].x === food.x &&
+      snake.positions[0].y === food.y
+    ) {
+      snake.positions.push({
+        x: tailPosition.x,
+        y: tailPosition.y,
+      });
+      score +=
+        snake.positions.length + 10;
+      generateFood();
     }
 
     updateSnakeinDOM();
   }, seconds * 10 + 250);
 };
 
-document.addEventListener("keydown", (event) => {
-  if (!isCaounting) return;
-  if (event.key === "ArrowRight") {
-    snake.direction = "right";
+document.addEventListener(
+  "keydown",
+  (event) => {
+    if (!isCaounting) return;
+    if (event.key === "ArrowRight") {
+      snake.direction = "right";
+    }
+    if (event.key === "ArrowLeft") {
+      snake.direction = "left";
+    }
+    if (event.key === "ArrowUp") {
+      snake.direction = "up";
+    }
+    if (event.key === "ArrowDown") {
+      snake.direction = "down";
+    }
   }
-  if (event.key === "ArrowLeft") {
-    snake.direction = "left";
-  }
-  if (event.key === "ArrowUp") {
-    snake.direction = "up";
-  }
-  if (event.key === "ArrowDown") {
-    snake.direction = "down";
-  }
-});
+);
